5d4b8590ca5a715bb2d2201b6b42193ce9a9223f
josephl37 <josephliow2000@gmail.com>
2023-05-13 18:51:11 -0500

    added restrict component

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,45 @@ import { useEffect, useState } from "react";
 import { fetchData } from "./redux/makerSlice";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "./redux/store";
+import { useAppSelector } from "./redux/store";
 
 import Header from "./components/header";
 import TopDB from "./components/top-db";
 import BottomDB from "./components/bottom-db";
 import Signature from "./components/signature";
+import Restricted from "./components/restricted";
 
 function App() {
   const [market, setMarket] = useState(1);
   const [interval, setInterval] = useState(3600);
+  const error = useAppSelector((state) => state.data.error);
 
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(fetchData({ market, interval }));
   }, [dispatch, market, interval]);
 
-  return (
-    <div className="mt-10 lg:w-5/6 sm:w-full lg:mx-auto mx-4">
-      <Header
-        market={market}
-        setMarket={setMarket}
-        interval={interval}
-        setInterval={setInterval}
-      />
-      <TopDB />
-      <BottomDB />
-      <Signature />
-    </div>
-  );
+  if (!error) {
+    return (
+      <div className="mt-10 lg:w-5/6 sm:w-full lg:mx-auto mx-4">
+        <Header
+          market={market}
+          setMarket={setMarket}
+          interval={interval}
+          setInterval={setInterval}
+        />
+        <TopDB />
+        <BottomDB />
+        <Signature />
+      </div>
+    );
+  } else {
+    return (
+      <div className="mx-10">
+        <Restricted />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/restricted.tsx
@@ -0,0 +1,53 @@
+const Restricted = () => {
+  return (
+    <div className="flex mt-40 justify-center">
+      <div className="bg-gray-3 border border-gray-2 w-96 h-auto">
+        <div className="text-white font-semibold text-xl border-b border-gray-2 px-4 py-2">
+          <p>Restricted Territory</p>
+        </div>
+        <div className="px-4 mt-4">
+          <p className="text-gray-1 text-xs">
+            Unfortunately, we are not able to support users from the following
+            restricted territories at this time:
+          </p>
+        </div>
+        <div className="text-gray-1 text-xs mx-4 my-4 border border-gray-2">
+          <div className="p-2">
+            <p>Balkans</p>
+            <p>Belarus</p>
+            <p>Burma (Myanmar)</p>
+            <p>Côte D’Ivoire (Ivory Coast)</p>
+            <p>Cuba</p>
+            <p>Democratic Republic of Congo</p>
+            <p>Iran</p>
+            <p>Iraq</p>
+            <p>Liberia</p>
+            <p>North Korea</p>
+            <p>Russia</p>
+            <p>Sudan</p>
+            <p>Syria</p>
+            <p>Ukraine (Crimea, Donetsk and Luhansk regions)</p>
+            <p>United States of America</p>
+            <p>Zimbabwe</p>
+          </div>
+        </div>
+        <div className="px-4 my-4">
+          <p className="text-gray-1 text-xs">
+            Please refer to the{" "}
+            <a
+              href="https://vertexprotocol.com/docs/TermsOfService.pdf"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+              Vertex Terms of Service
+            </a>{" "}
+            for more information.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Restricted;
